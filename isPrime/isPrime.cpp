48c998b594b3b423f9b76a020d8ee2035468388c
mshah4306 <mshah4306@gmail.com>
2021-07-09 15:45:09 +0500

    SLR-4: Removed code smells

--- isPrime/isPrime.cpp
+++ isPrime/isPrime.cpp
@@ -1,39 +1,24 @@
-//4# Prime Number
-
 #include <iostream>
 using namespace std;
 
 void main()
 {
 	int number = 0;
-	int a = 0;
-	int status = 1;
+	bool isPrime = true;
 	cout << "Please Enter a number" << endl;
 	cin >> number;
-	//a = number / 2;
-	if (number == 2)
-	{
-		cout << "Number is prime";
+	
+	for (int i = 2; i <= number / 2; i++) {
+		if (number % i == 0) {
+			isPrime = false;
+			break;
+		}
 	}
-	else
-	{
 
-		for (int i = 2; i <= number / 2; i++)
-		{
-			if (number % i == 0)
-			{
-				cout << "Number is not Prime." << endl;
-				status = 0;
-				break;
-			}
-		}
-		if (status)
-		{
-		/*	cout << "Number is not Prime";
-		}
-		else
-		{*/
-			cout << "Number is Prime";
-		}
+	if (isPrime) {
+		cout << "Number is Prime";
+	}
+	else {
+		cout << "Number is not Prime." << endl;
 	}
 }
