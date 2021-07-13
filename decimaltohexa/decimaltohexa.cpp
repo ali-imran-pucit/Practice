471fd97f6b20955fe9151ab6c07c346523963b6d
mshah4306 <mshah4306@gmail.com>
2021-07-13 16:46:04 +0500

    SLR-10 : Code updated

--- decimaltohexa/decimaltohexa.cpp
+++ decimaltohexa/decimaltohexa.cpp
@@ -10,35 +10,15 @@ void main()
 	cout << "Please enter a number" << endl;
 	cin >> number;
 
-	while (number > 1)
-	{
-		string remainder;
-		remainder = to_string(number % 16);
-		if (remainder == "10") {
-			remainder = "A";
-		}
-		else if (remainder == "11") {
-			remainder = "B";
-		}
-		else if (remainder == "12") {
-			remainder = "C";
-		}
-		else if (remainder == "13"){
-			remainder = "D";
-		}
-		else if (remainder == "14"){
-			remainder = "E";
-		}
-		else if (remainder == "15"){
-			remainder = "F";
-		}
-		hexa = remainder + hexa;
-		number /= 16;
-		if (number == 1)
-		{
-			hexa = to_string(number) + hexa;
-		}
+	if (number == 0) {
+			cout << "Hexa of given input is: 0";
 	}
-	cout << "Hexa of given Input is: " << hexa;
+	else {
+		while (number > 0)
+		{
+			hexa = (char)((number % 16) < 10 ? ((number % 16) + 48) : ((number % 16) + 55)) + hexa;
+			number /= 16;
+		}
+		cout << "Hexa of given input is: " << hexa;
+	}
 }
-
