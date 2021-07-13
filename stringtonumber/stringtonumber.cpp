79dec09998de94cb24632589722277d185f94db9
mshah4306 <mshah4306@gmail.com>
2021-07-13 16:52:53 +0500

    SLR-19 : code updated

--- stringtonumber/stringtonumber.cpp
+++ stringtonumber/stringtonumber.cpp
@@ -10,24 +10,21 @@ void main()
 	cin >> input;
 
 	bool valid = true;
-	string con;
+	int result = 0;
 
 	int length = input.length();
 	for (int i = 0; i < length; i++){
-		int a = 0;
-		a = input[i];
-		if (a >= 48 && a <= 57){
-			con = con + input[i];
-			valid = true;
+		if ((int)input[i] >= 48 && (int)input[i] <= 57){
+			result = (result * 10) + ((int)input[i] - 48);
 		}
 		else{
 			valid = false;
 			break;
 		}
 	}
+
 	if (valid){
-		int result = stoi(con);
-		cout <<result ;
+		cout << "The Result is : " << result;
 	}
 	else{
 		cout << "Please enter a valid input";
