b06a2015fa09e427b6d439d0833d1cac68bc7de5
mshah4306 <mshah4306@gmail.com>
2021-07-13 16:50:13 +0500

    SLR-12 : code updated

--- anybasetodecimal/anybasetodecimal.cpp
+++ anybasetodecimal/anybasetodecimal.cpp
@@ -9,62 +9,38 @@ void main()
 	int result = 0;
 	string basename;
 
-	cout << "Please enter a number" << endl;
+	cout << "please enter a number" << endl;
 	cin >> number;
 	cout << "please enter a base" << endl;
 	cin >> base;
 
 	if (base == 2 || base == 8 || base == 16) {
 		if (base == 2) {
-			basename = "Binary";
+			basename = "binary";
 		}
 		else if (base == 8) {
-			basename = "Octal";
+			basename = "octal";
 		}
 		else if (base == 16) {
-			basename = "HexaDecimal";
+			basename = "hexadecimal";
 		}
 
-		string con;
 		bool valid = true;
-		int length = number.length();
-		int power = length;
-		for (int i = 0; i < length; i++) {
-			int digit = 0;
-			int ascii = number[i];
-			con = con + number[i];
-			if (ascii >= 65 && ascii <= 70) {
-				if (ascii == 65) {
-					digit = 10;
+		int power = number.length() - 1;
+		for (int i = 0; i < number.length(); i++) {
+			if (((int)number[i] >= 48 && (int)number[i] <= 57) || ((int)number[i] >= 65 && (int)number[i] <= 70)) {
+				int digit = ((int)number[i] >= 48 && (int)number[i] <= 57) ? ((int)number[i] - 48) : ((int)number[i] - 55);
+				if (!(digit < base)) {
+					valid = false;
+					break;
 				}
-				else if (ascii == 66) {
-					digit = 11;
-				}
-				else if (ascii == 67) {
-					digit = 12;
-				}
-				else if (ascii == 68) {
-					digit = 13;
-				}
-				else if (ascii == 69) {
-					digit = 14;
-				}
-				else if (ascii == 70) {
-					digit = 15;
-				}
-			}
-			else {
-				digit = stoi(con);
-			}
-			con = "";
-			if ((base == 2 && ascii == 48 || ascii == 49) || (base == 8 && ascii >= 48 && ascii <= 57) || (base == 16 && ascii >= 48 && ascii <= 57 || ascii >= 65 && ascii <= 70)) {
-				power = power - 1;
 				int exponent = 1;
 				for (int i = 0; i < power; i++) {
 					exponent *= base;
 				}
-				result = result + (exponent * digit);
-				valid = true;
+
+				result += (digit * exponent);
+				power--;
 			}
 			else
 			{
@@ -74,15 +50,15 @@ void main()
 		}
 		if (valid)
 		{
-			cout << "Decimal of " << basename << " " << number << " is: " << result;
+			cout << "decimal of " << basename << " " << number << " is: " << result;
 		}
 		else
 		{
-			cout << "Enter a valid input of " << basename;
+			cout << "enter a valid input of " << basename;
 		}
 	}
 	else
 	{
-		cout << "Please Enter base from Binary '2', Octal '8' and Hexa '16' only";
+		cout << "please enter base from binary '2', octal '8' and hexa '16' only";
 	}
 }
